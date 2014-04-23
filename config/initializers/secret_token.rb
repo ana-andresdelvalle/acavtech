40939d159b301aa604f9077c77e3ff0ea1a3a208
Ana <acandresdelvalle@gmail.com>
2014-04-23 15:46:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Acavtech::Application.config.secret_key_base = '58a171db1e93a74ce577f91539b8af6133fcfb8cbd23bcf1a54270f2257c55947d7243705809fd9c0b847204bfae6acbf9059b41cf3973a04a6d1c5d2ed86ed3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Acavtech::Application.config.session_store :cookie_store, key: '_acavtech_session'
